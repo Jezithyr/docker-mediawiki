63d6d914c0b321743c68b5c586ac94046d2336cb
Jezithyr <Jezithyr@gmail.com>
2024-12-31 17:28:17 -0800

    removed multi wiki updating

--- scripts/run_update.sh
+++ scripts/run_update.sh
@@ -5,24 +5,11 @@ scriptDir=$(dirname "$0")
 cd $scriptDir
 cd ..
 if [ "$#" -ne 1 ]; then
-  echo "No service specified running update on ALL wikis" >&2
-  containerNames=$(docker compose ps --services)
-  for containerName in $containerNames; do
-    if [ "$(docker compose exec $containerName  test -f /var/www/mediawiki/maintenance/update.php && echo 'true')" = 'true' ]; then 
-		echo "Running update for $containerName"
-		docker compose exec $containerName  php /var/www/mediawiki/maintenance/update.php --quick
-	fi
-	done
+  echo "No wiki service specified, you must specify a wiki service to update" >&2
   cd $oldDir
   exit 0
 fi
 
 WIKISERVICE=$1
-if (docker compose exec $WIKISERVICE test -f /var/www/mediawiki/maintenance/update.php); then 
-	docker compose exec $WIKISERVICE php /var/www/mediawiki/maintenance/update.php --quick
-else
-	echo "$WIKISERVICE is not a mediawiki container"
-	cd $oldDir
-	exit 1
-fi
+docker compose exec $WIKISERVICE php /var/www/mediawiki/maintenance/update.php --quick
 cd $oldDir
